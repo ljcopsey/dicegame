31b9437dd9bfe3d11b705466a1cf63f3cace0cc2
Libby Copsey <libbycopsey@hotmail.co.uk>
2021-07-13 23:56:00 +0100

    Start on value iteration

--- dicegame_algo.py
+++ dicegame_algo.py
@@ -1,3 +1,6 @@
+from dice_game import DiceGame
+game = DiceGame()
+import numpy as np
 from abc import ABC, abstractmethod
 
 
@@ -52,181 +55,123 @@ if __name__ == "__main__":
     
     game = DiceGame()
     
-    agent1 = AlwaysHoldAgent(game)
-    play_game_with_agent(agent1, game, verbose=True)
+   # agent1 = AlwaysHoldAgent(game)
+    #play_game_with_agent(agent1, game, verbose=True)
     
     print("\n")
     
-    agent2 = PerfectionistAgent(game)
-    play_game_with_agent(agent2, game, verbose=True)
+    #agent2 = PerfectionistAgent(game)
+    #play_game_with_agent(agent2, game, verbose=True)
 
     class MyAgent(DiceGameAgent):
-    def __init__(self, game):
-        
-        """
-        if your code does any pre-processing on the game, you can do it here
-        
-        e.g. you could do the value iteration algorithm here once, store the policy, 
-        and then use it in the play method
-        
-        you can always access the game with self.game
-        """
-        # this calls the superclass constructor (does self.game = game)
-        super().__init__(game) 
-        
-        # YOUR CODE HERE
-        gamma = 0.9
-        delta = state - new_state
-        
-        def start_vals(self, state):
-            for v in values:
-                v = [0]
-                 
-        
-    def play(self, state):
-        """
-        given a state, return the chosen action for this state
-        at minimum you must support the basic rules: three six-sided fair dice
-        
-        if you want to support more rules, use the values inside self.game, e.g.
-            the input state will be one of self.game.states
-            you must return one of self.game.actions
-        
-        read the code in dicegame.py to learn more
-        """
-        # YOUR CODE HERE
-        
-        while delta > 0:
+        def __init__(self, game):
             
-        
-        
-        return (0, 1, 2)
+            """
+            if your code does any pre-processing on the game, you can do it here
+            
+            e.g. you could do the value iteration algorithm here once, store the policy, 
+            and then use it in the play method
+            
+            you can always access the game with self.game
+            """
+            # this calls the superclass constructor (does self.game = game)
+            super().__init__(game) 
+            
+            # YOUR CODE HERE
+            self.gamma = 0.9
+            self.theta = 1
 
-        SKIP_TESTS = True
+            #self.state_values
 
-if not SKIP_TESTS:
-    import time
+                    
+            
+        def play(self, state):
+            """
+            given a state, return the chosen action for this state
+            at minimum you must support the basic rules: three six-sided fair dice
+            
+            if you want to support more rules, use the values inside self.game, e.g.
+                the input state will be one of self.game.states
+                you must return one of self.game.actions
+            
+            read the code in dicegame.py to learn more
+            """
+            # YOUR CODE HERE
+            
+            print(game.get_dice_state())
+            self.val_iteration(state)
 
-    total_score = 0
-    total_time = 0
-    n = 10
 
-    np.random.seed()
+        def utility(self, state):
+            #print(game.final_score(game._current_dice))
+            return state[0] + state[1] + state[2]
 
-    print("Testing basic rules.")
-    print()
 
-    game = DiceGame()
+        def action_probabilities(self, state):
+            states, game.actions, reward, probabilities = game.get_next_states(() , (game.get_dice_state()))
+            for s, probability in zip(state, probabilities):
 
+                # return the total score of the dice for each action
+                t = game.final_score(state)
+                a = []
+                for s in state:
+                    # reward = -1 + gamma discounted value of new state
+                    r = reward + (s * self.gamma)
+                    a.append(t * r)
+            print(max(a, key=lambda x:float(x)))
+            return 
+            
+
+
+        def val_iteration(self, state):   
+            delta = 0
+            v = np.zeros(len(state), dtype=int)
+
+            while True:
+                for s in state:
+                    prev = v.copy()
+                    #v = np.max(game.get_next_states())
+                    x = self.action_probabilities(state)
+                    print(x)
+
+                    #Bellman's equation
+                    b = game.get_next_states
+                    print(b)
+
+                    #delta = max
+                    return 
+
+                if delta < self.theta:
+                    return (0, 1, 2)
+
+
+
+      #TESTING
+import time
+
+total_score = 0
+total_time = 0
+n = 10
+
+np.random.seed()
+
+print("Testing basic rules.")
+print()
+
+game = DiceGame()
+
+start_time = time.process_time()
+test_agent = MyAgent(game)
+total_time += time.process_time() - start_time
+
+for i in range(n):
     start_time = time.process_time()
-    test_agent = MyAgent(game)
+    score = play_game_with_agent(test_agent, game)
     total_time += time.process_time() - start_time
 
-    for i in range(n):
-        start_time = time.process_time()
-        score = play_game_with_agent(test_agent, game)
-        total_time += time.process_time() - start_time
+    print(f"Game {i} score: {score}")
+    total_score += score
 
-        print(f"Game {i} score: {score}")
-        total_score += score
-
-    print()
-    print(f"Average score: {total_score/n}")
-    print(f"Total time: {total_time:.4f} seconds")
-
-    TEST_EXTENDED_RULES = False
-
-if not SKIP_TESTS and TEST_EXTENDED_RULES:
-    total_score = 0
-    total_time = 0
-    n = 10
-
-    print("Testing extended rules – two three-sided dice.")
-    print()
-
-    game = DiceGame(dice=2, sides=3)
-
-    start_time = time.process_time()
-    test_agent = MyAgent(game)
-    total_time += time.process_time() - start_time
-
-    for i in range(n):
-        start_time = time.process_time()
-        score = play_game_with_agent(test_agent, game)
-        total_time += time.process_time() - start_time
-
-        print(f"Game {i} score: {score}")
-        total_score += score
-
-    print()
-    print(f"Average score: {total_score/n}")
-    print(f"Average time: {total_time/n:.5f} seconds")
-
-
-    import sys
-import pathlib
-
-fail = False;
-
-if not SKIP_GAME:
-    fail = True;
-    print("You must set the SKIP_GAME constant to True in the earlier cell.")
-
-if not SKIP_TESTS:
-    fail = True;
-    print("You must set the SKIP_TESTS constant to True in the earlier cell.")
-    
-p1 = pathlib.Path('./readme.txt')
-p2 = pathlib.Path('./readme.md')
-if not (p1.is_file() or p2.is_file()):
-    fail = True;
-    print("You must include a separate file called readme.txt or readme.md in your submission.")
-    
-p3 = pathlib.Path('./dicegame.ipynb')
-if not p3.is_file():
-    fail = True
-    print("This notebook file must be named dicegame.ipynb")
-    
-if "MyAgent" not in dir():
-    fail = True;
-    print("You must include a class called MyAgent as defined above.")
-else:    
-    game = DiceGame()
-    agent = MyAgent(game)
-    action = agent.play((1, 1, 1))
-
-    if action not in game.actions:
-        print("Warning:")
-        print("Your agent does not seem to produce a valid action with the default rules.")
-        print()
-        print("Your assignment is unlikely to get any marks from the autograder. While we will")
-        print("try to check it manually to assign some partial credit, we encourage you to ask")
-        print("for help on the forum or directly to a tutor.")
-        print()
-        print("Please use the readme file to explain your code anyway.")
-        
-    if TEST_EXTENDED_RULES:
-        print("Info: TEST_EXTENDED_RULES is ON")
-        game = DiceGame(dice=2, sides=8)
-        agent = MyAgent(game)
-        try:
-            action = agent.play((7, 8))
-        except:
-            action = None
-
-        if action not in game.actions:
-            fail = True
-            print("Your agent does not produce a valid action with the extended rules.")
-            print("Turn off TEST_EXTENDED_RULES if you cannot fix this error.")
-    else:
-        print("Info: TEST_EXTENDED_RULES is OFF (extended rules will not be tested)")
-    
-if fail:
-    print()
-    sys.stderr.write("Your submission is not ready! Please read and follow the instructions above.")
-else:
-    print()
-    print("All checks passed. When you are ready to submit, upload the notebook and readme file to the")
-    print("assignment page, without changing any filenames.")
-    print()
-    print("If you need to submit multiple files, you can archive them in a .zip file. (No other format.)")
+print()
+print(f"Average score: {total_score/n}")
+print(f"Total time: {total_time:.4f} seconds")
